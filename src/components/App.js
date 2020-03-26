2eac9013776076824cecaf3b1f120a6fad93217d
Michael Skeen <mmskeen@gmail.com>
2020-03-25 21:13:41 -0600

    finished exercise

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes.js";
 
 function App() {
+  const noteComponents = notes.map(n => (
+    <Note key={n.id} title={n.title} content={n.content} />
+  ));
   return (
     <div>
       <Header />
-      <Note />
+      {noteComponents}
       <Footer />
     </div>
   );
